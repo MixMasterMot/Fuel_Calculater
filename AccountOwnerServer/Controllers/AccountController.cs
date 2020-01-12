1829f1f93172ee6b5f3039c3796637dfd2b7ed11
MixMasterMot <ljbotha93@gmail.com>
2020-01-12 21:21:57 +0200

    Add project files.

---
+++ AccountOwnerServer/Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using Contracts;
+using Entities.DataTransferObjects;
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AccountOwnerServer.Controllers
+{
+    [Route("api/account")]
+    public class AccountController : ControllerBase
+    {
+        private ILoggerManager _logger;
+        private IRepositoryWrapper _repository;
+        private IMapper _mapper;
+
+        public AccountController(ILoggerManager logger, IRepositoryWrapper repository, IMapper mapper)
+        {
+            _logger = logger;
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllAccounts()
+        {
+            try
+            {
+                var accounts = _repository.Account.GetAllAccounts();
+                _logger.LogInfo($"Returned all accounts from database.");
+
+                var accountsResult = _mapper.Map<IEnumerable<AccountDto>>(accounts);
+                return Ok(accountsResult);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside GetAllAccounts action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAccountById(Guid id)
+        {
+            try
+            {
+                var acc = _repository.Account.GetAccountById(id);
+                if(acc == null)
+                {
+                    _logger.LogError($"Account with id: {id}, hasn't been found in db.");
+                    return NotFound();
+                }
+                else
+                {
+                    _logger.LogInfo($"Returned account with id: {id}");
+                    var accResult = _mapper.Map<AccountDto>(acc);
+                    return Ok(accResult);
+                }
+
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside GetAccountById action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
---
+++ Repository/AccountRepository.cs
@@ -0,0 +1,33 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Repository
+{
+    public class AccountRepository : RepositoryBase<Account>, IAccountRepository 
+    { 
+        public AccountRepository(RepositoryContext repositoryContext) 
+            : base(repositoryContext) 
+        { 
+        }
+
+        public IEnumerable<Account> AccountsByOwner(Guid ownerId)
+        { 
+            return FindByCondition(a => a.OwnerId.Equals(ownerId)).ToList(); 
+        }
+
+        public Account GetAccountById(Guid accId)
+        {
+            return FindByCondition(ac => ac.Id.Equals(accId)).FirstOrDefault();
+        }
+
+        public IEnumerable<Account> GetAllAccounts()
+        {
+            return FindAll().OrderBy(ac => ac.Id).ToList();
+        }
+
+    }
+}
